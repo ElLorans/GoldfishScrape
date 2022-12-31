c31f9cbefb90330da91dd3424520e1fef8371f08
ElLorans <lorenzo.cerreta@libero.it>
2022-12-31 08:18:04 +0100

    refactor

--- MtgScraper/GoldfishScraper.py
+++ MtgScraper/GoldfishScraper.py
@@ -7,32 +7,16 @@ main()
         scrape_cards()
             deck_text_to_dict()
 """
-from typing import Dict, Optional, Tuple
+from typing import Dict, Iterable, Optional, Tuple, Union
 
 import requests
 from bs4 import BeautifulSoup
 from tqdm.auto import tqdm
 
-from . import MtgBoard, MtgDeck
+from MtgScraper.mtg_utils import *
 
 
-def deck_text_to_dict(stringa: str) -> MtgBoard:
-    """
-    Convert plain text to dictionary. Text must be separated by \n and number of copies must be separated by ' ' from
-    card names.
-    :param stringa: '1 Card\n3 OtherCard\n...'
-    :return: {'Card': 1, 'OtherCard': 3...}
-    """
-    decklist = MtgBoard()
-    for line in stringa.splitlines():
-        if len(line.strip()) > 0:
-            copies, card = line.split(' ', 1)
-            card = card.split('[')[0].split('<')[0].strip()
-            decklist[card] = decklist.get(card, 0) + int(copies)
-    return decklist
-
-
-def grab_links(gf_html: str, clean=True) -> Dict[str, str]:
+def grab_links(gf_html: str, clean: bool = True) -> Dict[str, str]:
     """
     Return ORDERED dict of links to scrape; if param clean is True, decks named only after colours combinations are
         not included.
@@ -50,41 +34,14 @@ def grab_links(gf_html: str, clean=True) -> Dict[str, str]:
     soup = BeautifulSoup(non_budget, "lxml")
 
     names = soup.find_all('span', {'class': 'deck-price-paper'})[1:]
-    names_links = dict()
-    permutations = {'W', 'U', 'B', 'R', 'G', 'WU', 'WB', 'WR', 'WG', 'UW', 'UB', 'UR', 'UG', 'BW', 'BU', 'BR', 'BG',
-                    'RW', 'RU', 'RB', 'RG', 'GW', 'GU', 'GB', 'GR', 'WUB', 'WUR', 'WUG', 'WBU', 'WBR', 'WBG', 'WRU',
-                    'WRB', 'WRG', 'WGU', 'WGB', 'WGR', 'UWB', 'UWR', 'UWG', 'UBW', 'UBR', 'UBG', 'URW', 'URB', 'URG',
-                    'UGW', 'UGB', 'UGR', 'BWU', 'BWR', 'BWG', 'BUW', 'BUR', 'BUG', 'BRW', 'BRU', 'BRG', 'BGW', 'BGU',
-                    'BGR', 'RWU', 'RWB', 'RWG', 'RUW', 'RUB', 'RUG', 'RBW', 'RBU', 'RBG', 'RGW', 'RGU', 'RGB', 'GWU',
-                    'GWB', 'GWR', 'GUW', 'GUB', 'GUR', 'GBW', 'GBU', 'GBR', 'GRW', 'GRU', 'GRB', 'WUBR', 'WUBG', 'WURB',
-                    'WURG', 'WUGB', 'WUGR', 'WBUR', 'WBUG', 'WBRU', 'WBRG', 'WBGU', 'WBGR', 'WRUB', 'WRUG', 'WRBU',
-                    'WRBG', 'WRGU', 'WRGB', 'WGUB', 'WGUR', 'WGBU', 'WGBR', 'WGRU', 'WGRB', 'UWBR', 'UWBG', 'UWRB',
-                    'UWRG', 'UWGB', 'UWGR', 'UBWR', 'UBWG', 'UBRW', 'UBRG', 'UBGW', 'UBGR', 'URWB', 'URWG', 'URBW',
-                    'URBG', 'URGW', 'URGB', 'UGWB', 'UGWR', 'UGBW', 'UGBR', 'UGRW', 'UGRB', 'BWUR', 'BWUG', 'BWRU',
-                    'BWRG', 'BWGU', 'BWGR', 'BUWR', 'BUWG', 'BURW', 'BURG', 'BUGW', 'BUGR', 'BRWU', 'BRWG', 'BRUW',
-                    'BRUG', 'BRGW', 'BRGU', 'BGWU', 'BGWR', 'BGUW', 'BGUR', 'BGRW', 'BGRU', 'RWUB', 'RWUG', 'RWBU',
-                    'RWBG', 'RWGU', 'RWGB', 'RUWB', 'RUWG', 'RUBW', 'RUBG', 'RUGW', 'RUGB', 'RBWU', 'RBWG', 'RBUW',
-                    'RBUG', 'RBGW', 'RBGU', 'RGWU', 'RGWB', 'RGUW', 'RGUB', 'RGBW', 'RGBU', 'GWUB', 'GWUR', 'GWBU',
-                    'GWBR', 'GWRU', 'GWRB', 'GUWB', 'GUWR', 'GUBW', 'GUBR', 'GURW', 'GURB', 'GBWU', 'GBWR', 'GBUW',
-                    'GBUR', 'GBRW', 'GBRU', 'GRWU', 'GRWB', 'GRUW', 'GRUB', 'GRBW', 'GRBU', 'WUBRG', 'WUBGR', 'WURBG',
-                    'WURGB', 'WUGBR', 'WUGRB', 'WBURG', 'WBUGR', 'WBRUG', 'WBRGU', 'WBGUR', 'WBGRU', 'WRUBG', 'WRUGB',
-                    'WRBUG', 'WRBGU', 'WRGUB', 'WRGBU', 'WGUBR', 'WGURB', 'WGBUR', 'WGBRU', 'WGRUB', 'WGRBU', 'UWBRG',
-                    'UWBGR', 'UWRBG', 'UWRGB', 'UWGBR', 'UWGRB', 'UBWRG', 'UBWGR', 'UBRWG', 'UBRGW', 'UBGWR', 'UBGRW',
-                    'URWBG', 'URWGB', 'URBWG', 'URBGW', 'URGWB', 'URGBW', 'UGWBR', 'UGWRB', 'UGBWR', 'UGBRW', 'UGRWB',
-                    'UGRBW', 'BWURG', 'BWUGR', 'BWRUG', 'BWRGU', 'BWGUR', 'BWGRU', 'BUWRG', 'BUWGR', 'BURWG', 'BURGW',
-                    'BUGWR', 'BUGRW', 'BRWUG', 'BRWGU', 'BRUWG', 'BRUGW', 'BRGWU', 'BRGUW', 'BGWUR', 'BGWRU', 'BGUWR',
-                    'BGURW', 'BGRWU', 'BGRUW', 'RWUBG', 'RWUGB', 'RWBUG', 'RWBGU', 'RWGUB', 'RWGBU', 'RUWBG', 'RUWGB',
-                    'RUBWG', 'RUBGW', 'RUGWB', 'RUGBW', 'RBWUG', 'RBWGU', 'RBUWG', 'RBUGW', 'RBGWU', 'RBGUW', 'RGWUB',
-                    'RGWBU', 'RGUWB', 'RGUBW', 'RGBWU', 'RGBUW', 'GWUBR', 'GWURB', 'GWBUR', 'GWBRU', 'GWRUB', 'GWRBU',
-                    'GUWBR', 'GUWRB', 'GUBWR', 'GUBRW', 'GURWB', 'GURBW', 'GBWUR', 'GBWRU', 'GBUWR', 'GBURW', 'GBRWU',
-                    'GBRUW', 'GRWUB', 'GRWBU', 'GRUWB', 'GRUBW', 'GRBWU', 'GRBUW'}
+    names_links: Dict[str, str] = dict()
 
     for elem in names:
         name = elem.text.replace('\n', '')
 
         if name not in names_links and name != 'Other':
             # if deck name already present do NOT insert it in result dict
-            if clean and name not in permutations:  # exclude decks with bad names
+            if clean and name not in unclear_deck_names:  # exclude decks with bad names
                 names_links[name] = "https://www.mtggoldfish.com" + elem.a["href"]
 
             elif not clean:
@@ -107,11 +64,12 @@ def scrape_cards(html_soup: BeautifulSoup) -> Tuple[MtgBoard, Optional[MtgBoard]
     return deck_text_to_dict(main_text), None
 
 
-def scrape_deck_page(html_deck: str) -> (str, MtgBoard, MtgBoard):
+def scrape_deck_page(html_deck: str, deck_name: Optional[str] = None) -> (str, MtgBoard, MtgBoard):
     """
     Get Tuple with Deck Name, Mainboard dict and Sideboard dict from html of deck page.
-
-    :param: html_deck: html page of deck e.g.: https://www.mtggoldfish.com/archetype/standard-jeskai-fires#paper
+    TODO: Improve Deck Name (unreliable since Nov 2020)
+    :param html_deck: html page of deck e.g.: https://www.mtggoldfish.com/archetype/standard-jeskai-fires#paper
+    :param deck_name:
     :return: tuple (deck_name: str, mainboard: dict, sideboard: dict)
     """
     soup = BeautifulSoup(html_deck, "lxml")
@@ -122,73 +80,87 @@ def scrape_deck_page(html_deck: str) -> (str, MtgBoard, MtgBoard):
     #   Four - Color Omnath
     #   <span class ="author"> by VTCLA </span>
     # </h1>
-    deck_name = soup.find("span", {"class": "author"}).previousSibling.replace('\n', '').strip()
-    deck_name = deck_name.replace("/", "").replace("-", " ").replace("\n\nSuggest\xa0a\xa0Better\xa0Name", "").replace(
-        "\nFix Archetype", "")
+    if deck_name is None:
+        deck_name = soup.find("span", {"class": "author"}).previousSibling.replace('\n', '').strip()
+        deck_name = deck_name.replace("/", "").replace("-", " ").replace("\n\nSuggest\xa0a\xa0Better\xa0Name",
+                                                                         "").replace("\nFix Archetype", "")
 
-    for sign in ("[", "<", "{"):
-        deck_name = deck_name.split(sign)[0]
-
+        for sign in ("[", "<", "{"):
+            deck_name = deck_name.split(sign)[0]
     main, side = scrape_cards(soup)
     return deck_name, main, side
 
 
-def main(formato: str, full=True) -> (Dict[str, int], Dict[str, int]):
+def build_url(formato: str, full: bool = True):
+    url_start = "https://www.mtggoldfish.com/metagame/"
+    url_end = "/full#paper" if full is True else "/#paper"
+    return url_start + formato + url_end
+
+
+def scrape_formato(formato: str,
+                   full: bool = True,
+                   session: Optional[requests.Session] = None,
+                   limit: Optional[int] = None,
+                   already_scraped: Optional[Iterable] = None) -> (
+        Dict[str, int], Dict[str, int]):
     """
     Get dict of mains and dict of sides. 
     :formato: str (e.g.: "standard" or "modern")
     :full: bool, True for scraping all decks from /full#paper,
            False for scraping only first decks from /#paper url)
+    :session:
+    :already_scraped:
     :return:
     """
     mainboards = dict()
     sideboards = dict()
 
-    url_start = "https://www.mtggoldfish.com/metagame/"
-    url_end = "/full#paper" if full is True else "/#paper"
-    url = url_start + formato + url_end
+    url = build_url(formato, full)
     print(f"Getting links from {url}")
 
-    with requests.Session() as session:
-        page = session.get(url)
-        links = grab_links(page.text)  # .values()
+    if session is None:
+        session = requests.Session()
+    with session as connection:
+        page = connection.get(url)
+        links = grab_links(page.text)
         print(f"{len(links)} links grabbed!!\n")
-
+        if limit is not None:
+            links = {k: v for k, v in list(links.items())[:limit]}
         for link_name, link in tqdm(links.items()):
+            if already_scraped and link_name in already_scraped:
+                continue
             # print(f"Getting data from:\n{link}")
-            try:
-                page = session.get(link).text
-                name, mainb, side = scrape_deck_page(page)
-                # from Nov 2020, name from grab_links is more correct than scrape_deck_page
-                name = link_name
-                if name in mainboards:
-                    print(f"{name} is a CONFLICTING NAME and will not be saved.")
-                elif len(mainb) == 0:
-                    print(f"{name} has EMPTY MAINBOARD and will not be saved.")
-                else:
-                    mainboards[name] = mainb
-                    sideboards[name] = side
-            except TimeoutError:
-                print("The connection FAILED due to a TimeOut Error.")
-            except AttributeError as e:
-                print("AttributeError", e, "at", url)
-            except Exception as e:
-                print(e, "\n", link, "will not be scraped")
-                import pdb
-                pdb.set_trace()
+
+            page = connection.get(link).text
+            # from Nov 2020, name from grab_links is more correct than scrape_deck_page,
+            # so pass it to function directly
+            name, mainb, side = scrape_deck_page(page, link_name)
+            if name in mainboards:
+                print(f"{name} is a CONFLICTING NAME and will not be saved.")
+            elif len(mainb) == 0:
+                print(f"{name} has EMPTY MAINBOARD and will not be saved.")
+            else:
+                mainboards[name] = mainb
+                sideboards[name] = side
+
     return mainboards, sideboards
 
 
 class MtgGoldfishScraper:
-    def __init__(self, fullness=False):
+    def __init__(self, fullness=False, session=None):
         self.fullness = fullness
+        if session is None:
+            self.session = requests.Session()
+        else:
+            self.session = session
 
     def scrape_formats(self,
-                       formats=(
-                       'Standard', 'Modern', 'Pioneer', 'Pauper', 'Legacy', 'Vintage', 'Commander_1v1', 'Commander')
+                       formats: Union[list[str], tuple[str], set[str]] = (
+                               'Standard', 'Modern', 'Pioneer', 'Pauper', 'Legacy',
+                               'Vintage', 'Commander_1v1', 'Commander')
                        ):
-        formats_type = type(formats)
-        assert isinstance(formats, str), "formats should be an iterable but not a str"
+        if isinstance(formats, str):  # if formats were a str, function would scrape a url for each letter
+            formats = (formats,)
         scraped = dict()
         scraped_sideboards = dict()
 
@@ -204,7 +176,7 @@ class MtgGoldfishScraper:
     def scrape_format(self, mtg_format, fullness=None):
         if fullness is None:
             fullness = self.fullness
-        return main(mtg_format, fullness)
+        return scrape_formato(mtg_format, fullness)
 
     def scrape_page(self):
         pass
@@ -215,9 +187,9 @@ if __name__ == '__main__':
 
     example_url = "https://www.mtggoldfish.com/metagame/standard/full#paper"
     response = requests.get(example_url)
-    links = grab_links(response.text)
+    deck_links = grab_links(response.text)
 
     pdb.set_trace()
-    page_html = requests.get(list(links.values())[0]).text
+    page_html = requests.get(list(deck_links.values())[0]).text
     decklist = scrape_deck_page(page_html)
     print(decklist)
